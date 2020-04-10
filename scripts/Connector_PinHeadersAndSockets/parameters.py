368b8efd874ad529e1188ea1a3e08ec3536800ac
persus <github@susic.info>
2020-04-10 13:26:14 +0200

    Initial commit.

---
+++ scripts/Connector_PinHeadersAndSockets/parameters.py
@@ -0,0 +1,14 @@
+### 
+
+from collections import namedtuple
+
+### use enums (Phyton 3+)
+
+class CaseType:
+    THT = 'THT'
+    SMD = 'SMD'
+
+class PinStyle:
+    STRAIGHT = 'Straight'
+    ANGLED   = 'Angled' 
+
